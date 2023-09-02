20571d24be4962949531590d73c7c5da4499f61c
Luis Moreno <luismoreno2103@gmail.com>
2023-09-02 13:06:23 -0500

    removed token

--- bot.py
+++ bot.py
@@ -19,4 +19,19 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+<<<<<<< HEAD
 client.run(os.getenv('BOT_TOKEN'))
+=======
+        # Convert joined_at to the server's timezone
+        joined_at = joined_at.astimezone(timezone)
+
+        current_time = datetime.datetime.now(timezone)
+        days_in_server = (current_time - joined_at).days
+
+        await ctx.send(f'{user.display_name} has been in the server {days_in_server} days.')
+    else:
+        await ctx.send('This command is only applicable in a server (guild) context')
+
+
+bot.run('BOT_TOKEN')
+>>>>>>> 8caf7c1 (removed token)
