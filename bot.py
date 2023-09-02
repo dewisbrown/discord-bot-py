308327404ae30b261bb96af36dfc45a2897a81ca
Luis Moreno <luismoreno2103@gmail.com>
2023-09-02 13:20:55 -0500

    nothing changed

--- bot.py
+++ bot.py
@@ -1,27 +1,29 @@
 import discord
+import random
 from discord.ext import commands
-import os
+import datetime
+import pytz
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello, I am your Discord bot!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def age(ctx, user: discord.Member = None):
+    if user is None:
+        user = ctx.author
+    
+    if ctx.guild:
+        joined_at = user.joined_at
+        timezone = pytz.timezone('America/Chicago')
 
-<<<<<<< HEAD
-client.run(os.getenv('BOT_TOKEN'))
-=======
         # Convert joined_at to the server's timezone
         joined_at = joined_at.astimezone(timezone)
 
@@ -34,4 +36,3 @@ client.run(os.getenv('BOT_TOKEN'))
 
 
 bot.run('BOT_TOKEN')
->>>>>>> 8caf7c1 (removed token)
