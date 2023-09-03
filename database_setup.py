4d4cb16ff06b429ae12a58e7325858231551f729
Luis Moreno <luismoreno2103@gmail.com>
2023-09-02 19:04:11 -0500

    creates database for users and server points

---
+++ database_setup.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# Connect to sqlite database (make new if doesn't exist)
+conn = sqlite3.connect('data/points.db')
+
+# Create a cursor object to execute SQL commands
+cursor = conn.cursor()
+
+# Create a table to store member points
+cursor.execute('''CREATE TABLE IF NOT EXISTS points (
+                    user_id INTEGER PRIMARY KEY,
+                    points INTEGER DEFAULT 0,
+                    last_awarded_at TIMESTAMP
+                )''')
+
+# Commit and close the connection
+conn.commit()
+conn.close()
