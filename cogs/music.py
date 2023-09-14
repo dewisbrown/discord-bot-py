cedbc690ee7148f4f18d1c8d5950b2dddbffa33c
Luis Moreno <luismoreno2103@gmail.com>
2023-09-14 12:25:59 -0500

    attempting to get bot to join voice channel and play mp4

--- cogs/music.py
+++ cogs/music.py
@@ -1,10 +1,10 @@
 import discord
-import youtube_dl
 import asyncio
 import os
 import datetime
 from discord.ext import commands
 from discord.voice_client import VoiceClient
+from pytube import YouTube
 
 # List for song queue
 queue = []
@@ -14,7 +14,7 @@ class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         '''Print statment to ensure loads properly.'''
@@ -25,76 +25,65 @@ class MusicCog(commands.Cog):
     @commands.command()
     async def queue(self, ctx):
         '''Displays the music queue.'''
-        embed = discord.Embed(title=f'Current Queue | {len(queue)}', timestamp=datetime.datetime.now())
-        for index, item in enumerate(queue):
-            embed.add_field(name=f'{index + 1} | (`{item["song_duration"]}`) {item["song_name"]} - {item["request_author"]}', value='', inline=False)
-        await ctx.send(embed=embed)
+        note_emoji = '\U0001F3B5'
+        embed = discord.Embed(title=f'{note_emoji}  **Current Queue | {len(queue)} entries**', timestamp=datetime.datetime.now())
+        message = ''
 
+        for index, item in enumerate(queue):
+            message += f'`{index + 1}` | (`{item["song_duration"]}`) **{item["song_name"]} -** {item["request_author"]}\n'
+        
+        embed.add_field(name='', value=message, inline=False)
+        await ctx.send(embed=embed)
 
 
     @commands.command()
-    async def play(self, ctx, *, search_terms):
+    async def play(self, ctx, *, url):
         '''Plays the user submitted search terms in audio chat.'''
-        # Check if the user is in a voice channel
-        #if ctx.author.voice is None:
-            #await ctx.send("You need to be in a voice channel to use this command.")
-            #return
-
-        # Join the user's voice channel
-        channel = ctx.author.voice.channel
-
-        print(channel)
-
-        #voice_client = await channel.connect()
-
-        # Create a YouTube search URL based on user-supplied search terms
-        youtube_url = f"https://www.youtube.com/results?search_query={search_terms}"
-
-        # Set up the options for youtube_dl
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'outtmpl': 'audio_cache/%(title)s.%(ext)s',  # Cache audio files
-        }
-
-        # Use youtube_dl to fetch the audio URL
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(youtube_url, download=False)
-            url2play = info['entries'][0]['url']
-            song_name = info['entries'][0]['title']
-            song_duration = info['entries'][0]['duration']
-            thumbnail_url = info['entries'][0]['thumbnail']
+    
+        # pytube YouTube object with user submitted url
+        yt = YouTube(url)
+        song_name = yt.title
+        song_duration = format_time(yt.length)
+        thumbnail_url = yt.thumbnail_url
+        note_emoji = '\U0001F3B5'
 
         # Add song to queue
-        add_to_queue(song_name, song_duration, ctx.author.name, thumbnail_url)
-
-        # Play the audio
-        #voice_client.play(discord.FFmpegPCMAudio(url2play))
-
-        # Adjust embed title when queue is implemented
-        embed = discord.Embed(timestamp=datetime.datetime.now())
-        embed.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
-        embed.add_field(name=f'Now Playing: {song_name} - [`{song_duration}`]', value=f'Requested by - {ctx.author.name}', inline=False)
-        embed.set_thumbnail(url=thumbnail_url)
-
-        await ctx.send(embed=embed)
-
-        # Wait until the audio is finished playing, then disconnect from the voice channel
-        #while voice_client.is_playing():
-            #await asyncio.sleep(1)
-        #await voice_client.disconnect()
-
-        # Clean up cached audio files
-        os.remove(f'audio_cache/{info["title"]}.mp3')
+        queue.append({'song_name': song_name, 'song_duration': song_duration, 'request_author': ctx.author.name, 'thumbnail_url': thumbnail_url})
+        if len(queue) == 1:
+            await ctx.send(f'{note_emoji}  Added **{song_name} (`{song_duration}`)** to begin playing.')
+        else:
+            await ctx.send(f'{note_emoji}  **{song_name}** added to the queue (`{song_duration}`) - at position {len(queue)}')
 
 
-def add_to_queue(song_name, song_duration, request_author, thumbnail_url):
-    '''Adds songs to queue.'''
-    queue.append({'song_name': song_name, 'song_duration': song_duration, 'request_author': request_author, 'thumbnail_url': thumbnail_url})
+    
+    @commands.command()
+    async def ding(self, ctx):
+        '''Joins voice channel and plays ding noise.'''
+        if ctx.author.voice and ctx.author.voice.channel:
+            channel = ctx.author.voice.channel
+        
+            voice_channel = await channel.connect()
+
+            mp4_path = os.path.join(os.path.dirname(__file__), '..', 'downloads', 'Ding Sound Effect - No Copyright.mp4')
+
+            if os.path.exists(mp4_path):
+                voice_channel.play(discord.FFmpegPCMAudio(mp4_path))
+
+                while voice_channel.is_playing():
+                    await asyncio.sleep(1)
+
+                await voice_channel.disconnect()
+            else:
+                await ctx.send('File `Ding Sound Effect - No Copyright.mp4` not found.')
+        else:
+            await ctx.send('You need to be in a voice channel to use this command.')
+        
+        
+
+def format_time(seconds):
+    '''Formats total seconds to %M:%S format.'''
+    minutes, seconds = divmod(seconds, 60)
+    return f'{minutes}:{seconds:02d}'
 
 
 async def setup(bot):
