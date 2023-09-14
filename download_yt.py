77b5a607d38c59695197429c615b0f48ec9c6317
Luis Moreno <luismoreno2103@gmail.com>
2023-09-14 17:45:25 -0500

    added file_path to returned dictionary in download()

--- download_yt.py
+++ download_yt.py
@@ -6,9 +6,10 @@ def download(url, request_author):
     yt = YouTube(url)
     audio_stream = yt.streams.filter(only_audio=True).first()
     output_path = os.path.join(os.path.dirname(__file__), 'downloads')
-    audio_stream.download(output_path=output_path)
+    print(f'Output Path: {output_path}')
+    file_path = audio_stream.download(output_path=output_path)
 
-    return {'song_name': yt.title, 'song_duration': format_time(yt.length), 'request_author': request_author, 'thumbnail_url': yt.thumbnail_url}
+    return {'song_name': yt.title, 'song_duration': format_time(yt.length), 'request_author': request_author, 'thumbnail_url': yt.thumbnail_url, 'file_path': file_path}
 
 
 def format_time(seconds):
@@ -17,6 +18,14 @@ def format_time(seconds):
     return f'{minutes}:{seconds:02d}'
 
 
-def delete(song_title):
+def delete(file_path):
     '''Deletes audio file from downloads directory.'''
-    os.remove(f'downloads/{song_title}.mp4')
+    if os.path.exists(file_path):
+        try:
+            os.remove(file_path)
+            print(f'Succesfully deleted {file_path}')
+        except OSError as error:
+            print(f'Error deleting file: {error}')
+    else:
+        print('The file path does not exist.')
+        print(file_path)
