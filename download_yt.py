8791ad901c886911ee3c1c3f074dfb61b26313e1
Luis Moreno <luismoreno2103@gmail.com>
2023-09-14 16:56:36 -0500

    script to download youtube audio

---
+++ download_yt.py
@@ -0,0 +1,21 @@
+import os
+from pytube import YouTube
+
+def download(url, request_author):
+    '''Downloads YouTube video and returns YouTube video data.'''
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_stream.download(output_path='downloads')
+
+    return {'song_name': yt.title, 'song_duration': format_time(yt.length), 'request_author': request_author, 'thumbnail_url': yt.thumbnail_url}
+
+
+def format_time(seconds):
+    '''Formats total seconds to %M:%S format.'''
+    minutes, seconds = divmod(seconds, 60)
+    return f'{minutes}:{seconds:02d}'
+
+
+def delete(song_title):
+    '''Deletes audio file from downloads directory.'''
+    os.remove(f'downloads/{song_title}.mp4')
