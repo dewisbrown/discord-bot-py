36b84c3e9e46f2afdb18898ebe8f09226029df6c
Luis Moreno <luismoreno2103@gmail.com>
2023-09-14 16:55:25 -0500

    deleted test script

--- test.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-
-yt = YouTube('https://www.youtube.com/watch?v=vFo7hQDtjWc')
-
-audio_stream = yt.streams.filter(only_audio=True).first()
-
-audio_stream.download(output_path='downloads')
