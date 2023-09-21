5933d025139bedf6606c6f5042e637134b6357d2
Luis Moreno <luismoreno2103@gmail.com>
2023-09-21 12:47:26 -0500

    removed openai functionality and added chatbot command

--- cogs/chat.py
+++ cogs/chat.py
@@ -1,59 +1,68 @@
 import logging
-import os
-import requests
-from dotenv import load_dotenv
+import discord
+import datetime
+import asyncio
 from discord.ext import commands
+from chatbot import chatbot_interface, add_new_answer
+
 
 class ChatCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        load_dotenv()
 
 
     @commands.Cog.listener()
     async def on_ready(self):
+        '''Print statment to ensure loads properly.'''
         logging.info('Chat Cog loaded.')
 
 
     @commands.command()
-    async def gpt(self, ctx, *, prompt):
+    async def chat(self, ctx, *, prompt):
         '''Returns a response from chatGPT given a prompt.'''
+        logging.info('Chat command submitted by [%s]', ctx.author.name)
+        response = chatbot_interface(prompt)
         
-         # Define the API endpoint
-        api_url = 'https://api.openai.com/v1/chat/completions'
-
-        # Set your OpenAI API key
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {os.getenv("OPEN_AI_API_KEY")}'
-        }
-
-        # Construct the request payload
-        payload = {
-            "model": "gpt-3.5-turbo",
-            "messages": [{"role": "user", "content": prompt}],
-            "temperature": 0.7
-        }
-
+        if response:
+            await ctx.send(response)
+            return
+        else:
+            await ctx.send('I don\'t know the response to that. Teach me? **"yes"** or **"no"**')
+        
+        # Wait for user response
         try:
-            # Make the API request
-            response = requests.post(api_url, headers=headers, json=payload)
+            teach_message = await self.bot.wait_for(
+                'message',
+                check=lambda message: message.author == ctx.author and message.content.lower() in ['yes', 'no'],
+                timeout=30
+            )
+        except asyncio.TimeoutError:
+            await ctx.send('You took to long to respond. Chat input cancelled.')
+            logging.error('Response timeout for [%s]', ctx.author.name)
+            return
+        
+        if teach_message.content.lower() == 'yes':
+            await ctx.send(f'Provide the response for:\n*{prompt}*')
+        else:
+            await ctx.send('No response learned \U0001F63F')
+            return
 
-            # Check if the request was successful (status code 200)
-            if response.status_code == 200:
-                data = response.json()
+        # Wait for user answer
+        try:
+            answer_message = await self.bot.wait_for(
+                'message',
+                check=lambda message: message.author == ctx.author,
+                timeout=30
+            )
+        except asyncio.TimeoutError:
+            await ctx.send('You took to long to respond. Chat input cancelled.')
+            logging.error('Response timeout for [%s]', ctx.author.name)
+            return
+        
+        answer = answer_message.content
+        add_new_answer(prompt, answer)
 
-                # Extract the generated message from the response
-                if 'choices' in data and data['choices'][0]['message']:
-                    generated_message = data['choices'][0]['message']
-                    await ctx.send(f'Response from ChatGPT: {generated_message}')
-                else:
-                    await ctx.send('No response received from ChatGPT.')
-            else:
-                print(f'Error: {response.status_code} - {response.text}')
-        except Exception as e:
-            print(f'Error: {str(e)}')
-
+        await ctx.send(f'New response learned:\nQ: **{prompt}**\nA: *{answer}*')
 
 async def setup(bot):
     await bot.add_cog(ChatCog(bot))
