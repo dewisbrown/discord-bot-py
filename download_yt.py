7293eb922c8c5d52bbb58463d5d2c9d03909382a
Luis Moreno <luismoreno2103@gmail.com>
2023-09-26 18:08:57 -0500

    ran into NoneType error, fixed in pytube.cipher.py line 411

--- download_yt.py
+++ download_yt.py
@@ -3,13 +3,21 @@ from pytube import YouTube
 
 def download(url, request_author):
     '''Downloads YouTube video and returns YouTube video data.'''
-    yt = YouTube(url)
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    output_path = os.path.join(os.path.dirname(__file__), 'downloads')
-    file_path = audio_stream.download(output_path=output_path)
+    try:
+        yt = YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        output_path = os.path.join(os.path.dirname(__file__), 'downloads')
+        file_path = audio_stream.download(output_path=output_path)
 
-    return {'song_name': yt.title, 'song_duration': format_time(yt.length), 'request_author': request_author, 'thumbnail_url': yt.thumbnail_url, 'file_path': file_path}
-
+        return {
+            'song_name': yt.title, 
+            'song_duration': format_time(yt.length), 
+            'request_author': request_author, 
+            'thumbnail_url': yt.thumbnail_url, 
+            'file_path': file_path
+        }
+    except Exception as ex:
+        print(str(ex))
 
 def format_time(seconds):
     '''Formats total seconds to %M:%S format.'''
